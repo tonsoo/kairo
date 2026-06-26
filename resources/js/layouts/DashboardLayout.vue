c8ada938d4cc52e5b1e20aefdc4ed2423dce6925
Alysson <alyssuuuuu@outlook.com>
2026-06-26 14:55:14 -0300

    fixed left panel overflowing screen

--- resources/js/layouts/DashboardLayout.vue
+++ resources/js/layouts/DashboardLayout.vue
@@ -9,20 +9,20 @@ import LocaleSwitch from '@/components/LocaleSwitch.vue';
     <div class="min-h-screen bg-[#1e1f20] font-sans text-slate-300">
         <div class="flex min-h-screen">
             <aside
-                class="hidden w-72 shrink-0 border-r border-[#2e2f30] bg-[#18191a] md:flex md:flex-col"
+                class="sticky top-0 hidden h-screen w-72 shrink-0 self-start overflow-hidden border-r border-[#2e2f30] bg-[#18191a] md:flex md:flex-col"
             >
-                <div class="flex-1 px-4 py-6">
+                <div class="min-h-0 flex-1 overflow-y-auto px-4 py-6">
                     <DashboardSidebarNav />
                 </div>
 
-                <div class="space-y-4 px-4 pb-4">
+                <div class="space-y-4 border-t border-[#2e2f30] px-4 py-4">
                     <DashboardShiftAction />
                     <DashboardUserMenu />
                 </div>
             </aside>
 
             <main class="relative min-w-0 flex-1 bg-[#1e1f20]">
-                <div class="pt-8 px-8 flex justify-end">
+                <div class="flex justify-end px-8 pt-8">
                     <LocaleSwitch compact />
                 </div>
                 <slot />
